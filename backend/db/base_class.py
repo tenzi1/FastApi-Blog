2f18ed11965b7b67734def5c6d22cb9c3fa2cdcd
tshering <sherpatshering783@gmail.com>
2024-03-20 14:01:02 +0545

    feat: initial project and db configuration setup

---
+++ backend/db/base_class.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import as_declarative
+from typing import Any
+
+
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from core.config import settings
+from db.base_class import Base
+from db.session import engine
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
+
+
+@app.get("/")
+def home():
+    return {"msg": "hello fastapi"}
