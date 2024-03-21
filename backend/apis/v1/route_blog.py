f44dce8a0ece0b3786c63d89d4a9b8faa98444b4
tshering <sherpatshering783@gmail.com>
2024-03-21 08:22:52 +0545

    feat: retrieval of single blog object

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from db.session import get_db
 from schemas.blog import ShowBlog, CreateBlog
-from db.repository.blog import create_new_blog
+from db.repository.blog import create_new_blog, retrieve_blog
 
 router = APIRouter()
 
@@ -11,4 +11,15 @@ router = APIRouter()
 @router.post("/blogs", response_model=ShowBlog, status_code=status.HTTP_201_CREATED)
 async def create_blog(blog: CreateBlog, db: Session = Depends(get_db)):
     blog = create_new_blog(db=db, blog=blog, author_id=1)
+    return blog
+
+
+@router.get("/blog/{id}", response_model=ShowBlog)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    blog = retrieve_blog(id=id, db=db)
+    if not blog:
+        raise HTTPException(
+            detail=f"Blog with ID {id} does not exists.",
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
     return blog
