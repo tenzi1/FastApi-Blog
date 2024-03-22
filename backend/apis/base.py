0105239f5ebaebfd888bf46ec6eeac708899a419
tshering <sherpatshering783@gmail.com>
2024-03-22 08:25:27 +0545

    feat: authentication setup

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -1,7 +1,9 @@
 from apis.v1 import route_blog
+from apis.v1 import route_login
 from apis.v1 import router_user
 from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(router_user.router, prefix="", tags=["user"])
 api_router.include_router(route_blog.router, prefix="", tags=["blog"])
+api_router.include_router(route_login.router, prefix="", tags=["login"])
