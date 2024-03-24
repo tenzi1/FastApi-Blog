164b0a22ce718605bd1b89c515997601a61c0cb9
tshering <sherpatshering783@gmail.com>
2024-03-24 20:38:21 +0545

    feat: blog detail template

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -1,4 +1,5 @@
 from db.repository.blog import list_blogs
+from db.repository.blog import retrieve_blog
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
@@ -17,4 +18,12 @@ def home(
     blogs = list_blogs(skip=skip, limit=limit, db=db)
     return templates.TemplateResponse(
         "blog/home.html", {"request": request, "blogs": blogs}
+    )
+
+
+@router.get("/app/blog/{id}")
+def blog_detail(request: Request, id: int, db: Session = Depends(get_db)):
+    blog = retrieve_blog(id=id, db=db)
+    return templates.TemplateResponse(
+        "blog/detail.html", {"request": request, "blog": blog}
     )
